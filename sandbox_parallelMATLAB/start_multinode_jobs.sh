f2b3258f592bab61d30d5de0ba98fe02daf7a8a6
eliob <elio.balestrieri@gmail.com>
2023-04-13 14:52:13 +0200

    corrected MATLAB toy example for HPC

--- sandbox_parallelMATLAB/start_multinode_jobs.sh
+++ sandbox_parallelMATLAB/start_multinode_jobs.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1         # the number of tasks/processes per node
 #SBATCH --cpus-per-task=36         # the number cpus per task
 #SBATCH --partition=normal          # on which partition to submit the job
-#SBATCH --time=00:15:00             # the max wallclock time (time limit your job will run)
+#SBATCH --time=00:05:00             # the max wallclock time (time limit your job will run)
 #SBATCH --job-name=testParallelMATLAB    # the name of your job
 #SBATCH --mail-type=ALL             # receive an email when your job starts, finishes normally or is aborted
 #SBATCH --mail-user=elio.balestrieri@gmail.com # your mail address
@@ -18,4 +18,4 @@ module load matlab/R2022a
 THISJOB=$(sed -n $SLURM_ARRAY_TASK_ID'p' JobList.csv | cut -d ',' -f2)
 
 # START THE APPLICATION
-matlab -nodisplay -nosplash -nodesktop -r "x='$PASSINPUT'; NodeLevelFunc(x); exit;"
+matlab -nodisplay -nosplash -nodesktop -r "x='$THISJOB'; NodeLevelFunc(x); exit;"
