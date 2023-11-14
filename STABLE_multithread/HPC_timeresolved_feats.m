66915900e3f87f5ed1e154594ade69f31a05d3c1
eliob <elio.balestrieri@gmail.com>
2023-11-14 14:48:32 +0100

    patch for avoid re-computing feats alphaSPADE

--- STABLE_multithread/HPC_timeresolved_feats.m
+++ STABLE_multithread/HPC_timeresolved_feats.m
@@ -16,10 +16,30 @@ if ~isfolder(outdir)
     mkdir(outdir)
 end
 
-
 % define subject list 
 filelist = struct2table(dir(fullfile(indir , 'fullsegcleanmeg_*.mat'))); % '/remotedata/AgGross/AlphaSpade/Data/_meg'
 filenames = filelist.name;
+
+% define subject list already computed
+already_comp_subjs = struct2table(dir(fullfile(outdir , '*_timeresolved.mat'))); % '/remotedata/AgGross/AlphaSpade/Data/_meg'
+already_comp_subjs = already_comp_subjs.name;
+
+% exclude all the subjects that have already been computed
+pat= regexpPattern('\d');
+
+codes_pre = cellfun(@(x) extract(x, pat), filenames, 'UniformOutput', false);
+codes_post = cellfun(@(x) extract(x, pat), already_comp_subjs, 'UniformOutput', false);
+
+for iPre = 1:length(codes_pre)
+    codes_pre{iPre} = [codes_pre{iPre}{:}];
+end
+
+for iPost = 1:length(codes_post)
+    codes_post{iPost} = [codes_post{iPost}{:}];
+end
+
+already_comp_positions = ismember(codes_pre, codes_post);
+filenames = filenames(~already_comp_positions);
 
 % prepare subject number and thread parfor 
 nsubjs = length(filenames);
