e6da2c33a0b13bd610572f4725a035117591343f
eliob <elio.balestrieri@gmail.com>
2023-11-18 14:20:14 +0100

    timeresolved classification adapted to segmented files

--- STABLE_multithread/start_job_SingleScript.sh
+++ STABLE_multithread/start_job_SingleScript.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=1                   # the number of nodes you want to reserve
 #SBATCH --ntasks-per-node=1         # the number of tasks/processes per node
 #SBATCH --cpus-per-task=20          # the number cpus per task
-#SBATCH --partition=e0bm          # on which partition to submit the job
+#SBATCH --partition=long          # on which partition to submit the job
 #SBATCH --time=7-00:00:00             # the max wallclock time (time limit your job will run)
  
  
-#SBATCH --job-name=BetweenSubjsLEMON  # the name of your job
+#SBATCH --job-name=TimeResolved  # the name of your job
 #SBATCH --mail-type=ALL             # receive an email when your job starts, finishes normally or is aborted
 #SBATCH --mail-user=elio.balestrieri@gmail.com # your mail address
  
@@ -19,4 +19,4 @@ module load scikit-learn/1.1.2
 module load dask
 
 # START THE APPLICATION
-python HPC_betweensubjs_mdl_compare_LEMON_CVpart.py
+python HPC_withinsubj_timeresolved_alphaSPADE.py
