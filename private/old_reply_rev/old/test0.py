9b343be21db1b4d91e6551aaea6b006384c3b918
eliob <elio.balestrieri@gmail.com>
2025-01-07 16:05:00 +0100

    removed private

--- private/old_reply_rev/old/test0.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 28 17:02:23 2024
-
-@author: balestrieri
-"""
-
-print('start')
--- private/sandbox_decode/sandbox_mdls_across.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 19 14:39:46 2023
-
-@author: balestrieri
-"""
-
